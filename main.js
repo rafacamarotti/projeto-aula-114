3929a2934a7bf71b9a03e446e6dc2f791f9ef7d6
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-03-27 23:23:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function modelLoaded(){
 function gotPoses(results){
  if(results.length >0){
     console.log(results);
-    noseX = results[0].pose.nose.x-125;
+    noseX = results[0].pose.nose.x-50;
     noseY = results[0].pose.nose.y-55;
  }
 }
