9fb5e81cce134501f0dacc5d9146246ae0c31cf7
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-03-27 23:37:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function gotPoses(results){
  if(results.length >0){
     console.log(results);
     noseX = results[0].pose.nose.x-70;
-    noseY = results[0].pose.nose.y-55;
+    noseY = results[0].pose.nose.y-80;
  }
 }
 function draw(){
